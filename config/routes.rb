949f123dc19fa5dfcdac7acdbd655f3102be2e99
Re-he4i5 <kuronekoryohei@gmail.com>
2022-06-09 20:18:47 +0900

    [add]name_sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
 
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 
 end
