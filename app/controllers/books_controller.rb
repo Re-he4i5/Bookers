ca00a8628a2fcc449d0140aa7d66c5907e5154f1
Re-he4i5 <kuronekoryohei@gmail.com>
2022-06-11 15:44:48 +0900

    [add]create_destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,16 @@ class BooksController < ApplicationController
   end
 
   def create
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@new_book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def edit
@@ -21,6 +31,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
 
