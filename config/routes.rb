c191fadbeda5df1b5a2d180ee76eb865a146d5af
Re-he4i5 <kuronekoryohei@gmail.com>
2022-06-21 20:45:41 +0900

    [init]

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
 
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
+
   resources :users, only: [:index, :show, :edit, :update]
 
 end
